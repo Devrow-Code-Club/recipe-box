9f6a30a8e229641988ac99bbc2dee8094d9dc21e
Matthew Mone <mattmone@gmail.com>
2021-09-22 14:33:11 -0700

    add mv recipe type to extractor

--- bookmarklet.js
+++ bookmarklet.js
@@ -15,6 +15,7 @@
     if(location.host.match(/pamperedchef.com/)) return buildRecipeCard("#recipeName", "#rpIngredients li", '#rpDirections li')
     if(document.querySelector('.wprm-recipe-container')) return buildRecipeCard('.wprm-recipe-name', '.wprm-recipe-ingredient', '.wprm-recipe-instruction')
     if(document.querySelector('body.recipe-template-default')) return buildRecipeCard('.recipe-title', '.recipe-ingredients__list li', '.recipe-directions__list .recipe-directions__item')
+    if(document.querySelector('.mv-recipe-card')) return buildRecipeCard('.mv-create-title-primary', '.mv-create-ingredients li', '.mv-create-instructions li')
     return {"message": "Could not extract recipe."};
   }
   const recipeJSON = JSON.stringify(pullRecipeCard(), null, 2);
