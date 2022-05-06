ecbe621fe3cc67e4ed59a1768a225b942d4dada1
Matthew Mone <mattmone@gmail.com>
2022-05-05 19:00:55 -0700

    make my own

--- src/sw.js
+++ src/sw.js
@@ -1,3 +1,31 @@
-import "https://cdn.jsdelivr.net/npm/workbox-precaching/build/workbox-precaching.prod.js";
+const PRECACHE = 'precache';
+const precacheManifest = (self.__WB_MANIFEST);
+const entryToUrl = ({ url, revision }) => `${url}?v=${revision}`;
+const precacheUrls = precacheManifest.map((entry) => entryToUrl(entry));
+const cleanPrecacheUrls = precacheManifest.map(({url}) => url);
 
-this.workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+self.addEventListener('install', event => {
+    event.waitUntil(async () => {
+        const cache = await caches.open(PRECACHE);
+        return Promise.all(precacheManifest.map(entry => cache.add(entryToUrl(entry))));
+    });
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(async () => {
+        const cache = await caches.open(PRECACHE);
+        const urls = await cache.keys();
+        urls.forEach(url => {
+            if(precacheUrls.includes(url)) return;
+            cache.delete(url);
+        })
+    });
+});
+
+self.addEventListener('fetch', event => {
+    if(cleanPrecacheUrls.includes(event.request.url)) event.respondWith(async () => {
+        const cache = await caches.open(PRECACHE);
+        const url = entryToUrl(precacheManifest.find(entry => entry.url === event.request.url));
+        return cache.match(url);
+    })
+})
