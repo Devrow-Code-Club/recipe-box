191a93f806312ab25d0ff272210878b78cc96815
Matthew Mone <mattmone@gmail.com>
2022-05-05 18:23:02 -0700

    jsdeliver?

--- src/sw.js
+++ src/sw.js
@@ -1,3 +1,3 @@
-import { precacheAndRoute } from "https://cdn.skypack.dev/workbox-precaching?min";
+import { precacheAndRoute } from "https://cdn.jsdelivr.net/npm/workbox-precaching/build/workbox-precaching.prod.js";
 
 precacheAndRoute(self.__WB_MANIFEST);
