f8654f4451efb6d7649eafa765ed53d37bfea2cc
Matthew Mone <mattmone@gmail.com>
2022-05-06 09:04:10 -0700

    more debug, add json to precache

--- src/sw.js
+++ src/sw.js
@@ -1,40 +1,55 @@
-const PRECACHE = 'precache';
-const CACHE = 'cache';
-const precacheManifest = (self.__WB_MANIFEST);
-const entryToUrl = ({ url, revision }) => new URL(`${location.origin}/${url}?v=${revision}`).toString();
-const precacheManifold = Object.fromEntries(precacheManifest.map(entry => [new URL(`${location.origin}/${entry.url}`).toString(), entryToUrl(entry)]));
+const PRECACHE = "precache";
+const CACHE = "cache";
+const precacheManifest = self.__WB_MANIFEST;
+const entryToUrl = ({ url, revision }) =>
+  new URL(`${location.origin}/${url}?v=${revision}`).toString();
+const precacheManifold = Object.fromEntries(
+  precacheManifest.map((entry) => [
+    new URL(`${location.origin}/${entry.url}`).toString(),
+    entryToUrl(entry),
+  ])
+);
 
-self.addEventListener('install', event => {
-    console.log('installing');
-    event.waitUntil((async () => {
-        const cache = await caches.open(PRECACHE);
-        return Promise.all(precacheManifest.map(entry => cache.add(entryToUrl(entry)))).then(() => {
-            console.log('done install');
-            self.skipWaiting();
-        });
-    })());
+self.addEventListener("install", (event) => {
+  console.log("installing");
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(PRECACHE);
+      return Promise.all(
+        precacheManifest.map((entry) => cache.add(entryToUrl(entry)))
+      ).then(() => {
+        console.log("done install");
+        self.skipWaiting();
+      });
+    })()
+  );
 });
 
-self.addEventListener('activate', event => {
-    event.waitUntil((async () => {
-        self.clients.claim();
-        const cache = await caches.open(PRECACHE);
-        const urls = (await cache.keys()).map(request => new URL(request.url).toString());
-        const precacheUrls = Object.values(precacheManifold);
-        urls.forEach(url => {
-            if(!precacheUrls.includes(url)) cache.delete(url);
-        })
-    })());
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async () => {
+      self.clients.claim();
+      const cache = await caches.open(PRECACHE);
+      const urls = (await cache.keys()).map((request) =>
+        new URL(request.url).toString()
+      );
+      const precacheUrls = Object.values(precacheManifold);
+      urls.forEach((url) => {
+        if (!precacheUrls.includes(url)) cache.delete(url);
+      });
+    })()
+  );
 });
 
-self.addEventListener('fetch', event => {
-    event.waitUntil((async () => {
-    console.log('fetch request');
+self.addEventListener("fetch", (event) => {
+  event.waitUntil(
+    (async () => {
+      console.log("fetch request");
       let requestUrl = event.request.url;
       if (requestUrl === `${location.origin}/`)
         requestUrl = new URL(`${location.origin}/index.html`).toString();
       if (Object.keys(precacheManifold).includes(requestUrl)) {
-        console.log('precache response');
+        console.log("precache response");
         return event.respondWith(
           (async () => {
             const cache = await caches.open(PRECACHE);
@@ -45,16 +60,17 @@ self.addEventListener('fetch', event => {
           })()
         );
       }
-    console.log('cache check')
+      console.log("cache check");
       const cache = await caches.open(CACHE);
       const match = await cache.match(requestUrl);
       if (match) {
-          console.log('match found, cache response')
-          return event.respondWith(match);
+        console.log("match found, cache response", match);
+        return event.respondWith(match);
       }
       const response = await fetch(requestUrl);
       cache.put(requestUrl, response.clone());
-      console.log('match not found, caching -> network response');
+      console.log("match not found, caching -> network response");
       return event.respondWith(response);
-    })());
-})
+    })()
+  );
+});
--- workbox-config.cjs
+++ workbox-config.cjs
@@ -1,5 +1,6 @@
 module.exports = {
-    swSrc: "src/sw.js",
-    swDest: "dist/sw.js",
-    globDirectory: "dist/"
-}
+  swSrc: "src/sw.js",
+  swDest: "dist/sw.js",
+  globDirectory: "dist/",
+  globPatterns: ["**/*.{css,html,js,json}"],
+};
