a4a72145cd1a123e1f280b68ca647c6c9f19d199
Matthew Mone <mattmone@gmail.com>
2022-05-05 18:15:45 -0700

    try new precaching cdn

--- src/sw.js
+++ src/sw.js
@@ -1,3 +1,3 @@
-import { precacheAndRoute } from "https://cdn.skypack.dev/-/workbox-precaching@v6.5.3-ziwpTKtnrapUbpwm3Xjb/dist=es2019,mode=imports/optimized/workbox-precaching.js";
+import { precacheAndRoute } from "https://cdn.skypack.dev/workbox-precaching?min";
 
 precacheAndRoute(self.__WB_MANIFEST);
