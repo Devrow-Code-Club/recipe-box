dc1a851ec64007e0dca3c6938c356360064f41a8
Matthew Mone <mattmone@gmail.com>
2022-05-05 19:30:50 -0700

    fix for sw

--- src/sw.js
+++ src/sw.js
@@ -5,27 +5,28 @@ const precacheUrls = precacheManifest.map((entry) => entryToUrl(entry));
 const cleanPrecacheUrls = precacheManifest.map(({url}) => url);
 
 self.addEventListener('install', event => {
-    event.waitUntil(async () => {
+    console.log('installing');
+    event.waitUntil((async () => {
         const cache = await caches.open(PRECACHE);
-        return Promise.all(precacheManifest.map(entry => cache.add(entryToUrl(entry))));
-    });
+        return Promise.all(precacheManifest.map(entry => cache.add(entryToUrl(entry)))).then(() => console.log('done install'));
+    })());
 });
 
 self.addEventListener('activate', event => {
-    event.waitUntil(async () => {
+    event.waitUntil((async () => {
         const cache = await caches.open(PRECACHE);
         const urls = await cache.keys();
         urls.forEach(url => {
             if(precacheUrls.includes(url)) return;
             cache.delete(url);
         })
-    });
+    })());
 });
 
 self.addEventListener('fetch', event => {
-    if(cleanPrecacheUrls.includes(event.request.url)) event.respondWith(async () => {
+    if(cleanPrecacheUrls.includes(event.request.url)) event.respondWith((async () => {
         const cache = await caches.open(PRECACHE);
         const url = entryToUrl(precacheManifest.find(entry => entry.url === event.request.url));
         return cache.match(url);
-    })
+    })());
 })
